c4dbbb5d212ccf5598b02ff03e2b7224e1e25cc2
obeyer1 <2528803338@qq.com>
2025-04-23 15:04:40 +0800

    添加注释

---
+++ lab3_1.cpp
@@ -0,0 +1,13 @@
+//转华氏度
+#include<iostream>
+using namespace std;
+float tansformer(float F){
+    return (F-32)*5/9;
+}
+int main()
+{
+    float F;
+    cin>>F;
+    cout<<tansformer(F);
+    return 0;
+}
